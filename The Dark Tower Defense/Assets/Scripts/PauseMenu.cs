12d2b7727d177266937edeb07847f1b2ec2f20fb
Weston Baker <wbaker_98@live.com>
2021-03-16 18:12:12 -0700

    Pause Menu

---
+++ The Dark Tower Defense/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject ui;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ToggleMenu();
+    }
+
+    public void ToggleMenu()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+
+    public void Retry()
+    {
+        ToggleMenu();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        ToggleMenu();
+        Debug.Log("Go TO Menu");
+    }
+}
